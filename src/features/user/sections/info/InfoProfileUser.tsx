53b6962941301813503c036e88eaeb9a9f319d35
jccetinac <jccetinac@gmail.com>
2024-09-26 14:26:13 -0500

    info tabs

--- src/features/user/sections/info/InfoProfileUser.tsx
+++ src/features/user/sections/info/InfoProfileUser.tsx
@@ -4,22 +4,29 @@ import { Content } from '../../../../components/layout/components/Content';
 import { InfoUserSidebar } from './InfoSidebar';
 import { InfoUserDetails } from './InfoUserDetails';
 import { UserFriendsList } from '../friend/UserFriendsList';
+import { UserModel } from '../../../../models/userModel';
+import { friendModel } from '../../../../models/friendModel';
 
 interface InfoProfileUserProps {
-  userId: string;
+  currentUser: UserModel;
+  friends: friendModel[];
 }
 
 const InfoUser: React.FC<InfoProfileUserProps> = ({currentUser, friends}) => {
   const [selectedInfoSection, onSelectedInfoSection] = useState('Información general');
+
   return (
     <Content>
       <div className="row">
         <div className="col-md-4">
-          <InfoUserSidebar  selectedInfoSection={selectedInfoSection} onSelectedInfoSection={onSelectedInfoSection} />
-          
+          <InfoUserSidebar 
+            selectedInfoSection={selectedInfoSection}
+            onSelectedInfoSection={onSelectedInfoSection} />        
         </div>
         <div className="col-md-8">
-          <InfoUserDetails  currentUser={currentUser}/>
+          <InfoUserDetails
+          selectedInfoSection={selectedInfoSection}
+          currentUser={currentUser}/>
         </div>
       </div>
       <UserFriendsList friends={friends} />
--- src/features/user/sections/info/InfoUserDetails.tsx
+++ src/features/user/sections/info/InfoUserDetails.tsx
@@ -1,49 +1,65 @@
 import React from 'react';
+import { UserModel } from '../../../../models/userModel';
 import { InfoDetailUserItem } from './InfoDetailItem';
 
-export const InfoUserDetails: React.FC = ({currentUser}) => {
+
+type InfoProfileUserProps = {
+  currentUser: UserModel,
+  selectedInfoSection: string
+}
+export const InfoUserDetails: React.FC<InfoProfileUserProps> = ({currentUser, selectedInfoSection}) => {
+  console.log({currentUser})
   const infoDetails = [
     {
       icon: 'bi-briefcase',
-      title: 'Tecnologo en Radiología en Clínica Vida Ips',
+      title: currentUser?.occupation,
       subtitle: 'Desde el 7 de marzo de 2020 hasta la fecha',
       privacyIcon: 'bi-lock-fill',
+      categories: ['Empleo y formación', 'Información general'],
     },
     {
       icon: 'bi-mortarboard',
       title: 'Estudió Máster Desarrollo Web Full Stack + MultiCloud en UNIR - La Universidad Internacional de La Rioja en Colombia',
       subtitle: '',
       privacyIcon: 'bi-people-fill',
+      categories: ['Empleo y formación', 'Información general'],
     },
     {
       icon: 'bi-house',
-      title: 'Vive en Medellín',
+      title: `${currentUser?.address?.city}`,
       subtitle: '',
       privacyIcon: 'bi-people-fill',
+      categories: ['Lugares de residencia'],
     },
     {
       icon: 'bi-geo-alt',
-      title: 'De Boca De Pepé, Choco, Colombia',
-      subtitle: '',
+      title: ` ${currentUser?.address?.addressLine}, ${currentUser?.address?.state}`,
+      subtitle: `${currentUser?.address?.postCode}`,
       privacyIcon: 'bi-globe',
+      categories: ['Lugares de residencia', 'Información general', 'Información básica y de contacto'],
+    },
+    {
+      icon: 'bi-telephone',
+      title: currentUser?.phone,
+      subtitle: 'Celular',
+      privacyIcon: 'bi-people-fill',
+      categories: ['Información básica y de contacto', 'Información general'],
     },
     {
       icon: 'bi-plus-circle',
       title: 'Agrega tu situación sentimental',
       subtitle: '',
       privacyIcon: '',
-    },
-    {
-      icon: 'bi-telephone',
-      title: '321 6283458',
-      subtitle: 'Celular',
-      privacyIcon: 'bi-people-fill',
+      categories: ['Familia y relaciones','Información general'],
     },
   ];
 
+  const detailFiltered = infoDetails.filter(detail => 
+    detail?.categories?.includes(selectedInfoSection));
+
   return (
     <div className="info-details">
-      {infoDetails.map((detail, index) => (
+      {detailFiltered.map((detail, index) => (
         <InfoDetailUserItem
           key={index}
           icon={detail.icon}
